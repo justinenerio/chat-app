557776e7591ac95360881d0b869c491a035373c6
Justin Enerio <justin.jamisola@gmail.com>
2020-05-02 20:56:12 +0800

    Updated web release files

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1,19 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "8d03c0b23369de925eaef8943ed15be7",
+  "main.dart.js": "b7faee5aa9f270ec43fedb842176e738",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "index.html": "8beddb02f4b4be779de168eca5ea642d",
 "/": "8beddb02f4b4be779de168eca5ea642d",
 "manifest.json": "df7828c3e7986976ce48391482d83718",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "7a4ca57cb90c7e6595c77dcdae7521c9",
+"assets/LICENSE": "0c894bfde685ca75ebac24612fa5eea7",
 "assets/assets/images/chat_logo.png": "481da98d85453b0ab594caa0f39ccf3b",
-"assets/AssetManifest.json": "64434f1167cf75ce7e52b2e769645c3c"
+"assets/AssetManifest.json": "92239c557d89d4c1802ed7c20e91e067"
 };
 
 self.addEventListener('activate', function (event) {
